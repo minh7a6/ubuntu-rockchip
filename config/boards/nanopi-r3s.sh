24aff8a346d74c373840f56499044bc545d11a8a
Debian <minh7a6@vyos.192.168.10.1>
2024-11-03 05:41:36 +0000

    Added nanopi r3s

---
+++ config/boards/nanopi-r3s.sh
@@ -0,0 +1,18 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Nanopi R3S"
+export BOARD_MAKER="FriendlyElec"
+export BOARD_SOC="Rockchip RK3566"
+export BOARD_CPU="ARM Cortex A55"
+export UBOOT_PACKAGE="u-boot-nanopi-r3s-1"
+export UBOOT_RULES_TARGET="nanopi-r3s-rk3566"
+export COMPATIBLE_SUITES=("noble")
+export COMPATIBLE_FLAVORS=("server")
+
+function config_image_hook__nanopi-r3s() {
+    local rootfs="$1"
+    local overlay="$2"
+    local suite="$3"
+
+    return 0
+}
---
+++ packages/u-boot-nanopi-r3s/debian/targets.mk
@@ -0,0 +1,6 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+u-boot-rockchip_platforms += nanopi-r3s-rk3566
+nanopi-r3s-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
+nanopi-r3s-rk3566_bl31 := rk3568_bl31_v1.43.elf
+nanopi-r3s-rk3566_pkg := nanopi-r3s
